e4aefc9ae3cf7682c20d5a948c2cf24b9d6463a9
Yerai <yespinar.moreno@knowmadmood.com>
2026-04-10 09:12:37 +0200

    change language of app from header

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { SelectButtonModule } from 'primeng/selectbutton';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -20,11 +20,18 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './header.css',
 })
 export class Header {
-  protected readonly title = 'TechPoC';
-  search: string = '';
-  stateOptions: { label: string; value: string }[] = [
-    { label: 'ES', value: 'ESP' },
-    { label: 'EN', value: 'ENG' },
+  protected readonly title = 'PostsListApp';
+  private readonly translateService = inject(TranslateService);
+  search = '';
+
+  languageOptions = [
+    { label: 'ES', value: 'es' },
+    { label: 'EN', value: 'en' },
   ];
-  value: string = 'ESP';
+  languageSelected = signal('es');
+
+  onLanguageChange(language: string): void {
+    this.languageSelected.set(language);
+    this.translateService.use(language);
+  }
 }
