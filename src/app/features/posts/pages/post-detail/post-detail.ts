b4e5563a40c0df5f0c1004e68eade5e956f6824a
Yerai <yespinar.moreno@knowmadmood.com>
2026-04-30 08:36:29 +0200

    manage event emited when a comment is created to update list of comments

--- src/app/features/posts/pages/post-detail/post-detail.ts
+++ src/app/features/posts/pages/post-detail/post-detail.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -8,13 +8,16 @@ import { IPost } from '../../interfaces/post';
 import { AvatarModule } from 'primeng/avatar';
 import { Comment } from '../../components/comment/comment';
 import { TranslatePipe } from '@ngx-translate/core';
+import { AddCommentForm } from '../../components/add-comment-form/add-comment-form';
+import { IComment } from '../../interfaces/comment';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.html',
-  imports: [CommonModule, RouterModule, AvatarModule, Comment, TranslatePipe],
+  imports: [CommonModule, RouterModule, AvatarModule, Comment, TranslatePipe, AddCommentForm],
 })
 export class PostDetail {
+  readonly comments = signal<IComment[]>([]);
   private readonly route = inject(ActivatedRoute);
   private readonly postsService = inject(PostsService);
 
@@ -27,18 +30,32 @@ export class PostDetail {
     { initialValue: null },
   );
 
-  readonly comments = toSignal(
-    this.route.paramMap.pipe(
-      map((params) => Number(params.get('id'))),
-      switchMap((id) => this.postsService.getCommentsByPostId(id)),
-    ),
-    { initialValue: [] }
-  );
-
   readonly authorName = computed(() => {
     const author = this.users().find((user) => user.id === this.post()?.userId);
     return author ? author.name.charAt(0).toUpperCase() + author.name.slice(1) : 'Unknown';
   });
 
   readonly authorAvatar = computed(() => this.users().find((user) => user.id === this.post()?.userId)?.avatar ?? '');
+
+  constructor() {
+    this.route.paramMap.subscribe((params) => {
+      const postId = Number(params.get('id'));
+      this.loadComments(postId);
+    });
+  }
+
+  loadComments(postId: number): void {
+    this.postsService.getCommentsByPostId(postId).subscribe({
+      next: (comments) => {
+        const sortedComments = comments.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+        this.comments.set(sortedComments);
+      },
+    });
+  }
+
+  reloadComments(): void {
+    const postId = this.post()?.id;
+    if (!postId) return;
+    this.loadComments(postId);
+  }
 }
